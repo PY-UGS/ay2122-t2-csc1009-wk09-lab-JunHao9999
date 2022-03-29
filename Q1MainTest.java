41d1cc717a585ea6867e146b75148ee931d6fe8b
JunHao9999 <92625469+JunHao9999@users.noreply.github.com>
2022-03-29 09:48:46 +0800

    update

--- Q1MainTest.java
+++ Q1MainTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static java.lang.Character.isLetterOrDigit;
@@ -6,19 +7,38 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Q1MainTest {
     Wk09Q1 q1=new Wk09Q1();
-    @Test   //test if return is char
+    @RepeatedTest(15)    //test if return is char
     void testRandomCharacter() {
-        assertTrue(isLetterOrDigit(q1.getRandomCharacter()));
+        char testChar=q1.getRandomCharacter();
+        System.out.println("Q1 value: "+testChar);
+        boolean isLD = isLetterOrDigit(testChar);
+        if (isLD==true){
+            System.out.println(testChar+" is character");
+        }
+        else{
+            System.out.println(testChar+" is character");
+        }
+        assertTrue(isLetterOrDigit(testChar));
     }
-    @Test //is Prime
+    @RepeatedTest(15) //is Prime
     void testRandomDigitCharacter(){
         int value=(int)q1.getRandomDigitCharacter()-48; //to get actual value
+        System.out.println("Q2 value: "+value);
+        boolean isPrimeBool=isPrime(value);
+        if (isPrimeBool==true){
+            System.out.println(value+" is prime");
+        }
+        if (isPrimeBool==false){
+            System.out.println(value+" is not prime");
+        }
         assertTrue(isPrime(value));
     }
 
     public boolean isPrime(int num){
-        if (num<=1)
-                return false;
+        if (num<=0)
+            return false;
+        if (num==1)
+                return true;
         for(int i=2;i<num;i++){
             if (num%i==0) //divisable therefore not prime
                 return false;
